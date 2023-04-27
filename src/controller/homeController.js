5def6bb984ab99e89fb07afd2e4732e987e77659
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2023-04-27 16:27:03 +0700

    route & MVC

---
+++ src/controller/homeController.js
@@ -0,0 +1,6 @@
+const getHomePage = (req, res) => {
+  //logic
+  res.render("index.ejs");
+};
+
+export { getHomePage };
--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./route/web";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
+// set view engine
 configViewEngine(app);
 
-app.get("/", function (req, res) {
-  res.render("index.ejs");
-});
-
-app.get("/about", (req, res) => {
-  res.send("Hello Phuong Minh!");
-});
+// init web route
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
