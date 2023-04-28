e167688fb85d385ebfb93054a6f93b2cc14001ef
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2023-04-28 09:48:50 +0700

    connect database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,20 @@
-const getHomePage = (req, res) => {
+import connection from "../configs/connectDB";
+
+const getHomePage = async (req, res) => {
   //logic
-  res.render("index.ejs");
+  const data = [];
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    results.map((row) =>
+      data.push({
+        id: row.id,
+        firstName: row.firstName,
+        lastName: row.lastName,
+        email: row.email,
+        address: row.address,
+      })
+    );
+    res.render("index.ejs", { dataUsers: JSON.stringify(data) });
+  });
 };
 
 export { getHomePage };
